4e46feb914f0be4bef11175f46062b18b563fd33
dd <abc0970870@gmail.com>
2025-06-21 14:53:23 +0800

    chore: code renew

---
+++ US_EFT_Yahoo.py
@@ -0,0 +1,14 @@
+import yfinance as yf
+import pandas as pd
+# 擴充為迴圈
+# 美股六大ETF
+ticker = ['SPY','VOO','IVV','VTI','QQQ','VT']
+start_date = '2015-05-01'
+end_date = pd.Timestamp.today().strftime('%Y-%m-%d')
+
+for r in ticker:
+    df = yf.download(r, start=start_date, end=end_date)
+    df = df.reset_index() # 把日期變成一般欄位
+    df = df.drop(df.index[0])
+    csv_name = f"{r}.csv"
+    df.to_csv(csv_name, encoding="utf-8", index=False)
