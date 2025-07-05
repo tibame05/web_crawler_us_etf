c44f7f6c566a9db4235c75afce7d9975a32ecc4b
asleave <asleave@gmail.com>
2025-07-05 11:11:21 +0800

    V1

---
+++ database/config.py
@@ -0,0 +1,6 @@
+import os
+
+MYSQL_HOST = os.environ.get("MYSQL_HOST", "127.0.0.1")
+MYSQL_PORT = int(os.environ.get("MYSQL_PORT", 3306))
+MYSQL_ACCOUNT = os.environ.get("MYSQL_ACCOUNT", "root")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "test")
